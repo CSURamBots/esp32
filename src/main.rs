f81d32390ab65c568a0041667d05cced6cf2da9c
Shizcow <pohl.devin@gmail.com>
2021-10-25 22:10:50 -0600

    Rebased into a new example, fixed some building stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use embedded_svc::anyerror::*;
+use esp_idf_hal::prelude::*;
+use esp_idf_svc::sysloop::*;
+use std::{thread, time::Duration};
+
+fn main() -> anyhow::Result<()> {
+    loop {
+        println!("Hello world");
+        thread::sleep(Duration::from_secs(1));
+    }
 }
