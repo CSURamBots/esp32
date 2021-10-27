e45f7833109dc61eed1dbb35d79f92704c40f398
Shizcow <pohl.devin@gmail.com>
2021-10-27 17:10:08 -0600

    Fixed remote build to use sockets (can abort properly)

--- build_remote.sh
+++ build_remote.sh
@@ -8,6 +8,9 @@ TMP_DIR=$(mktemp -d)
 SSH_CFG=$TMP_DIR/ssh-cfg
 SSH_SOCKET=$TMP_DIR/ssh-socket
 
+SOCKET_NAME=esp32-build-vpc1
+SOCKET_FILE=/tmp/.ssh-$SOCKET_NAME
+
 # Create a temporary SSH config file:
 cat > "$SSH_CFG" <<ENDCFG
 Host *
@@ -16,10 +19,26 @@ Host *
 ENDCFG
 
 # Open a SSH tunnel:
-ssh -F "$SSH_CFG" -f -N -l $TARGET_USER $TARGET_ADDRESS
+if test -f "$SOCKET_FILE"; then
+	ssh -S "$SOCKET_FILE" -O exit "$SOCKET_NAME"
+fi
+
+ssh -S "$SOCKET_FILE" -M -F "$SSH_CFG" -f -N -l $TARGET_USER $TARGET_ADDRESS
+
+echo "Waiting for build server to compile"
+
+kill_ssh()
+{
+	echo "Killing ssh tunnel"
+	ssh -S "$SOCKET_FILE" -O exit "$SOCKET_NAME"
+	exit 0
+}
+
+trap kill_ssh SIGINT
 
 while true; do
-	ssh -F "$SSH_CFG" $TARGET_USER@$TARGET_ADDRESS "echo \"$TARGET_PATH\" | entr -npz sleep 1 && echo 'reflashing'"
-	scp -F "$SSH_CFG" $TARGET_USER@$TARGET_ADDRESS:"$TARGET_PATH" "$HOST_PATH"
+	ssh -S "$SOCKET_FILE" -F "$SSH_CFG" $TARGET_USER@$TARGET_ADDRESS "echo \"$TARGET_PATH\" | entr -npz sleep 1 && echo 'reflashing'"
+	# the following causes a permission error but without it the tunnel dies
+	scp -S "$SOCKET_FILE" -F "$SSH_CFG" $TARGET_USER@$TARGET_ADDRESS:"$TARGET_PATH" "$HOST_PATH"
 	cargo make run
 done
